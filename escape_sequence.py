145d3735b7dc98fa0858d3de756c07a4dfd6eba7
Shahed808 <107167352+Shahed808@users.noreply.github.com>
2022-07-04 17:08:34 +0530

    Add files via upload

---
+++ escape_sequence.py
@@ -0,0 +1,72 @@
+''' escape sequences '''
+# print('\f hii')                                                         # hii
+
+# print('hello wo\'rld')                                                  # hello wo'rld
+
+# print("""coding\n
+# environment""")                                                         # coding      environment
+
+
+# print("hii\anderson")                                                      # hiinderson (beep sound will produce)
+# print('hii\\anderson')                                                      # hii\anderson (no beep sound)
+# print('hii\bobby')                                                         # hiobby () (clears one character from the lefthand side)
+# print('hii\\bobby')                                                         # hii\bobby () (clears one character from the lefthand side)
+# print('hii\cruise')                                                        # hii\cruise
+# print('hii\dormano')                                                       # hii\dormano
+# print('hii\ezra')                                                          # hii\ezra
+# print('hii\fernandaz')                                                      # hii  ernandaz  (new line)
+# print('hii\geevan')                                                         # hii\geevan
+# print('hii\harman')                                                         # hii\harman
+# print('hii\incognito')                                                      # hi\incognito
+# print('hii\jordan')                                                         # hii\jordan
+# print('hii\kalam')                                                          # hii\kalam
+# print('hii\loki')                                                            # hii\loki
+# print('hii\mohammad')                                                        # hii\mohammad
+# print('hii\natasha')                                                         # hii    atasha (new line)
+# print('hii\oliver')                                                          # hii\oliver
+# print('hii\peter')                                                           # hii\peter
+# print('hii\quill')                                                           # hii\quill
+# print('hii\robert')                                                          # obert  (the righthand side characters from the \r will replace with the characters before \r and print rest of the characters on rightside )
+# print('hii\stark')                                                           # hii\stark
+# print('hii\tony')                                                            # hii    ony (expandtabs method logic)
+# print('hii\uzlel')                                                           # syntaxError (unicode)
+# print('hii\varsha')                                                          # hii arsha (new line)
+# print('hii\walker')                                                          # hii\walker
+# print('hii\xavier')                                                          # syntaxError (unicode)
+# print('hii\yasir')                                                           # hii\yasir
+# print('hii\zara')                                                            # hii\zara
+
+
+
+# print('\a')
+
+
+# print("hii\Anderson")                                                        # hii\Anderson
+# print('hii\\Anderson')                                                       # hii\Anderson
+# print('hii\Bobby')                                                           # hii\Bobby
+# print('hii\\Bobby')                                                          # hii\Bobby
+# print('hii\Cruise')                                                          # hii\Cruise
+# print('hii\Dormano')                                                         # hii\Dormano
+# print('hii\Ezra')                                                            # hii\Ezra
+# print('hii\Fernandaz')                                                       # hii\Fernandaz 
+# print('hii\Geevan')                                                          # hii\Geevan
+# print('hii\Harman')                                                          # hii\Harman
+# print('hii\Incognito')                                                       # hi\Incognito
+# print('hii\Jordan')                                                          # hii\Jordan
+# print('hii\Kalam')                                                           # hii\Kalam
+# print('hii\Loki')                                                            # hii\Loki
+# print('hii\Mohammad')                                                        # hii\Mohammad
+# print('hii\Natasha')                                                         # SyntaxError: (unicode error)
+# print('hii\\Natasha')                                                        # hii\Natasha
+# print('hii\Oliver')                                                          # hii\Oliver
+# print('hii\Peter')                                                           # hii\Peter
+# print('hii\Quill')                                                           # hii\Quill
+# print('hii\Robert')                                                          # hii\Robert  
+# print('hii\Stark')                                                           # hii\Stark
+# print('hii\Tony')                                                            # hii\Tony (expandtabs method logic)
+# print('hii\Uzlel')                                                           # syntaxError (unicode)
+# print('hii\Varsha')                                                          # hii\Varsha 
+# print('hii\Walker')                                                          # hii\Walker
+# print('hii\Xavier')                                                            # hii\Xavier
+# print('hii\Yasir')                                                             # hii\Yasir
+# print('hii\Zara')                                                              # hii\Zara
