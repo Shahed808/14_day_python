d56e7da94d6dbb4f1bca27843639120e851ed7b0
Shahed808 <107167352+Shahed808@users.noreply.github.com>
2022-07-04 17:07:52 +0530

    Add files via upload

---
+++ binary_octal_hexa_decimal_forms.py
@@ -0,0 +1,37 @@
+'''binary forms
+we will divide the value with 2'''
+
+# print(bin(22))                                              # 0b10110
+
+# print(0b11011)                                              # 27
+
+# print(0B10111)                                                # 23
+
+''' octal forms
+we will divide the value with 8'''
+
+# print(oct(99))                                               # 0o143
+
+# print(0O1011)                                                # 521
+
+
+''' hexadecimal from
+a/A - 10
+b/B - 11
+c/C - 12
+d/D - 13
+e/E - 14
+f/F - 15  
+we will divide the value with 16'''
+
+
+print(hex(71))                                                  # 0x47
+
+print(0xa)                                                      # 10
+print(0xb)                                                      # 11
+print(0xe)                                                      # 14
+print(0xf)                                                      # 15
+
+print(0o123)                                                      # 83
+print(0x123)                                                      # 291
+print(0b1111)                                                     # 15
